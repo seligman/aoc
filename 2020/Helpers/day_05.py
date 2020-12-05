923c0fe44a553d4cfabda4ea3c0457bcb925e5d5
Scott Seligman <scott.seligman@gmail.com>
2020-12-04 21:55:15 -0800

    And an animation for day 5

--- 2020/Helpers/day_05.py
+++ 2020/Helpers/day_05.py
@@ -5,9 +5,17 @@ from collections import defaultdict
 def get_desc():
     return 5, 'Day 5: Binary Boarding'
 
-def calc(log, values, mode):
+def calc(log, values, mode, draw=False):
     neighbors = defaultdict(int)
     seats = set()
+
+    if draw:
+        from grid import Grid
+        grid = Grid()
+        for x in range(8):
+            for y in range(111):
+                grid.set('.', x, y)
+
     for value in values:
         row, row_part = 127, 64
         seat, seat_part = 7, 4
@@ -18,16 +26,38 @@ def calc(log, values, mode):
             elif cur in {"L", "R"}:
                 seat += seat_part * {"L": -1, "R": 1}[cur]
                 seat_part //= 2
+
+        if draw:
+            grid.set("#", seat // 2, row // 2)
+            grid.save_frame()
+
         seat_id = (row // 2) * 8 + (seat // 2)
 
         seats.add(seat_id)
         neighbors[seat_id - 1] += 1
         neighbors[seat_id + 1] += 1
 
+    if draw:
+        me = [x for x in neighbors if neighbors[x] == 2 and x not in seats][0]
+
+        grid.set("target", me % 8, me // 8)
+        grid.save_frame()
+
+        grid.draw_frames(repeat_final=30)
+        Grid.make_animation(file_format="mp4", output_name="animation_%02d" % (get_desc()[0],))
+
     if mode == 1:
         return max(seats)
     else:
         return [x for x in neighbors if neighbors[x] == 2 and x not in seats][0]
+
+def other_draw(describe, values):
+    if describe:
+        return "Animate this"
+    else:
+        from dummylog import DummyLog
+        calc(DummyLog(), values, 1, draw=True)
+
 
 def test(log):
     values = log.decode_values("""
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ For the most part these are Python 3.x scripts.  In each year, you can run `adve
 solution and see its output.  On some days where it makes sense, you can also run `advent.py test 1` to run 
 through some of the test data.
 
-For 2019 I've uploaded the animated visualizations visualizations created under animations.
+For 2019 and 2020 I've uploaded the animated visualizations visualizations created under animations.
