b9783f27226d1a26bfc66cdecdda967309ee8ab6
Scott Seligman <scott.seligman@gmail.com>
2020-12-04 21:37:14 -0800

    Clean up code a bit

--- 2020/Helpers/day_05.py
+++ 2020/Helpers/day_05.py
@@ -1,49 +1,38 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
+
 def get_desc():
     return 5, 'Day 5: Binary Boarding'
 
 def calc(log, values, mode):
-    final = []
-
-    ret = 0
+    neighbors = defaultdict(int)
+    seats = set()
     for value in values:
         row, row_part = 127, 64
         seat, seat_part = 7, 4
         for cur in value:
-            if cur == "F":
-                row -= row_part
+            if cur in {"F", "B"}:
+                row += row_part * {"F": -1, "B": 1}[cur]
                 row_part //= 2
-            elif cur == "B":
-                row += row_part
-                row_part //= 2
-            elif cur == "L":
-                seat -= seat_part
+            elif cur in {"L", "R"}:
+                seat += seat_part * {"L": -1, "R": 1}[cur]
                 seat_part //= 2
-            elif cur == "R":
-                seat += seat_part
-                seat_part //= 2
+        seat_id = (row // 2) * 8 + (seat // 2)
 
-        row //= 2
-        seat //= 2
-
-        seat_id = row * 8 + seat
-        final.append(seat_id)
-        ret = max(ret, seat_id)
+        seats.add(seat_id)
+        neighbors[seat_id - 1] += 1
+        neighbors[seat_id + 1] += 1
 
     if mode == 1:
-        return ret
+        return max(seats)
     else:
-        final.sort()
-        for i in range(len(final)-1):
-            if final[i] + 2 == final[i+1]:
-                return final[i] + 1
+        return [x for x in neighbors if neighbors[x] == 2 and x not in seats][0]
 
 def test(log):
     values = log.decode_values("""
         FBFBBFFRLR
     """)
-
     log.test(calc(log, values, 1), 357)
 
 def run(log, values):
