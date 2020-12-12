1c2fcec88e6991d59be8e8d73b5f440198d1c8a7
Scott Seligman <scott.seligman@gmail.com>
2020-12-11 21:27:55 -0800

    Complex numbers make this cleaner

--- 2020/Helpers/day_12.py
+++ 2020/Helpers/day_12.py
@@ -4,13 +4,13 @@ def get_desc():
     return 12, 'Day 12: Rain Risk'
 
 def calc(log, values, mode):
-    x, y = 0, 0
-    wp_x, wp_y = 10, -1
+    pos = complex(0, 0)
+    way = complex(10, -1)
     dirs = {
-        'E': [1, 0, 'N', 'S'],
-        'S': [0, 1, 'E', 'W'],
-        'W': [-1, 0, 'S', 'N'],
-        'N': [0, -1, 'W', 'E'],
+        'E': [complex(1, 0), 'N', 'S'],
+        'S': [complex(0, 1), 'E', 'W'],
+        'W': [complex(-1, 0), 'S', 'N'],
+        'N': [complex(0, -1), 'W', 'E'],
     }
 
     if mode == 1:
@@ -19,35 +19,32 @@ def calc(log, values, mode):
             cardinal = cur[0]
             if cardinal == 'L':
                 for _ in range(0, int(cur[1:]), 90):
-                    dir = dirs[dir][2]
+                    dir = dirs[dir][1]
                 cardinal = "x"
             elif cardinal == 'R':
                 for _ in range(0, int(cur[1:]), 90):
-                    dir = dirs[dir][3]
+                    dir = dirs[dir][2]
                 cardinal = "x"
             elif cardinal == 'F':
                 cardinal = dir
             else:
                 cardinal = cur[0]
             if cardinal != "x":
-                x += dirs[cardinal][0] * int(cur[1:])
-                y += dirs[cardinal][1] * int(cur[1:])
+                pos += dirs[cardinal][0] * int(cur[1:])
     else:
         for cur in values:
             if cur[0] == "F":
-                x += wp_x * int(cur[1:])
-                y += wp_y * int(cur[1:])
+                pos += way * int(cur[1:])
             elif cur[0] in dirs:
-                wp_x += dirs[cur[0]][0] * int(cur[1:])
-                wp_y += dirs[cur[0]][1] * int(cur[1:])
+                way += dirs[cur[0]][0] * int(cur[1:])
             elif cur[0] == "R":
                 for _ in range(0, int(cur[1:]), 90):
-                    wp_x, wp_y = -wp_y, wp_x
+                    way = complex(-way.imag, way.real)
             elif cur[0] == "L":
                 for _ in range(0, int(cur[1:]), 90):
-                    wp_x, wp_y = wp_y, -wp_x
+                    way = complex(way.imag, -way.real)
 
-    return abs(x) + abs(y)
+    return int(abs(pos.real) + abs(pos.imag))
 
 def test(log):
     values = log.decode_values("""
