a1dd2be7168109a12db5b9848ab462becea96bcd
Scott Seligman <scott.seligman@gmail.com>
2020-12-14 22:33:42 -0800

    Speed up a bit

--- 2020/Helpers/day_15.py
+++ 2020/Helpers/day_15.py
@@ -9,23 +9,16 @@ def calc(log, values, mode):
     numbers = [int(x) for x in values[0].split(",")]
 
     said = {}
-    last = 0
+    last = None
 
-    for i in range(2020 if mode == 1 else 30000000):
-        if len(numbers) > 0:
-            if i > 0:
-                said[last] = i - 1
-            last = numbers.pop(0)
-        else:
-            if last in said:
-                temp = said[last]
-                said[last] = i - 1
-                last = i - temp - 1
-            else:
-                said[last] = i - 1
-                last = 0
+    for i in range(len(numbers)):
+        said[last], last = i, numbers[i]
+
+    for i in range(len(numbers), 2020 if mode == 1 else 30000000):
+        said[last], last = i, i - said.get(last, i)
         if (mode == 1 and i % 1000 == 19) or (mode == 2 and i % 5000000 == 999999):
             log.show(f"For round {i:8d}, {last:8d} was said")
+
     return last
 
 def test(log):
