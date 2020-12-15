37efbd7012ab471aff986d8659e90176162c5103
Scott Seligman <scott.seligman@gmail.com>
2020-12-14 21:23:49 -0800

    Finish off day 15

---
+++ 2020/Helpers/day_15.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict, deque
+
+def get_desc():
+    return 15, 'Day 15: Rambunctious Recitation'
+
+def calc(log, values, mode):
+    numbers = [int(x) for x in values[0].split(",")]
+
+    said = {}
+    last = 0
+
+    for i in range(2020 if mode == 1 else 30000000):
+        if len(numbers) > 0:
+            if i > 0:
+                said[last] = i - 1
+            last = numbers.pop(0)
+        else:
+            if last in said:
+                temp = said[last]
+                said[last] = i - 1
+                last = i - temp - 1
+            else:
+                said[last] = i - 1
+                last = 0
+        if i % 1000000 == 0:
+            log.show(f"For round {i:8d}, {last} was said")
+    return last
+
+def test(log):
+    values = log.decode_values("""
+        0,3,6
+    """)
+
+    log.test(calc(log, values, 1), 436)
+    # log.test(calc(log, values, 2), 175594)
+
+def run(log, values):
+    log(calc(log, values, 1))
+    log(calc(log, values, 2))
