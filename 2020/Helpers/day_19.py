e32f4ae941fdf7b0af39a505bf26a80a788dafcc
Scott Seligman <scott.seligman@gmail.com>
2020-12-19 10:29:55 -0800

    Get rid of recursion

--- 2020/Helpers/day_19.py
+++ 2020/Helpers/day_19.py
@@ -19,33 +19,34 @@ def calc(log, values, mode):
                 rules[cur[0]] = "( " + cur[1].strip() + " )"
 
     if mode == 2:
-        rules["8"] = "( 42 | 42 8 )"
-        rules["11"] = "( 42 31 | 42 11 31 )"
+        rules["8"] = "( 42 + )"
+        temp = []
+        for i in range(1, 10):
+            temp.append(f"42 {{{i}}} 31 {{{i}}}")
+        rules["11"] = "( " + " | ".join(temp) + " )"
 
     compiled = []
     for key in rules if mode == 1 else ["0"]:
-        def decode(temp, used):
+        def decode(temp):
             ret = []
             for x in temp:
                 if x in tail:
                     ret.append(tail[x])
-                elif x in rules and used[x] < 5:
-                    used2 = used.copy()
-                    used2[x] += 1
-                    ret += decode(rules[x].split(' '), used2)
+                elif x in rules:
+                    ret += decode(rules[x].split(' '))
                 else:
                     ret.append(x)
             return ret
 
-        rule = "".join(decode(rules[key].split(' '), defaultdict(int)))
-        rule = rule.replace(' ', '')
+        rule = "".join(decode(rules[key].split(' ')))
         compiled.append(re.compile("^" + rule.replace(" ", "") + "$"))
 
     count = 0
     for cur in [x for x in values if ":" not in x and len(x) > 0]:
-        if len([x for x in [x.match(cur) for x in compiled] if x is not None]) > 0:
-            count += 1
-
+        for rule in compiled:
+            if rule.search(cur) is not None:
+                count += 1
+                break
     return count
 
 def test(log):
