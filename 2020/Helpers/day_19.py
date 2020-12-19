76317aa6a271ec6a0cb9c6a34b0049b2925f35b0
Scott Seligman <scott.seligman@gmail.com>
2020-12-19 12:32:03 -0800

    No more recursion

--- 2020/Helpers/day_19.py
+++ 2020/Helpers/day_19.py
@@ -19,11 +19,8 @@ def calc(log, values, mode):
                 rules[cur[0]] = "( " + cur[1].strip() + " )"
 
     if mode == 2:
-        rules["8"] = "( 42 + )"
-        temp = []
-        for i in range(1, 10):
-            temp.append(f"42 {{{i}}} 31 {{{i}}}")
-        rules["11"] = "( " + " | ".join(temp) + " )"
+        rules["8"] = "(?P<r8> 42 + )"
+        rules["11"] = "(?P<r11> 42 + 31 + )"
 
     compiled = []
     for key in rules if mode == 1 else ["0"]:
@@ -41,11 +38,30 @@ def calc(log, values, mode):
         rule = "".join(decode(rules[key].split(' ')))
         compiled.append(re.compile("^" + rule.replace(" ", "") + "$"))
 
+    r42 = "".join(decode(rules["42"].split(' ')))
+    r31 = "".join(decode(rules["31"].split(' ')))
+    r42 = re.compile("^" + r42.replace(" ", ""))
+    r31 = re.compile(r31.replace(" ", "") + "$")
+
     count = 0
     for cur in [x for x in values if ":" not in x and len(x) > 0]:
         for rule in compiled:
-            if rule.search(cur) is not None:
-                count += 1
+            m = rule.search(cur)
+            if m is not None:
+                if mode == 1:
+                    count += 1
+                else:
+                    temp = m.group("r8") + m.group("r11")
+                    c42 = 0
+                    c31 = 0
+                    while r42.search(temp):
+                        c42 += 1
+                        temp = r42.sub("", temp)
+                    while r31.search(temp):
+                        c31 += 1
+                        temp = r31.sub("", temp)
+                    if c31 < c42:
+                        count += 1
                 break
     return count
 
