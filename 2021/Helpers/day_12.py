64d6e2a9f2d010a1b101f21696f2cee25e3052aa
Scott Seligman <scott.seligman@gmail.com>
2021-12-11 21:24:51 -0800

    Clean up

--- 2021/Helpers/day_12.py
+++ 2021/Helpers/day_12.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python3
 
-from collections import deque
+from collections import deque, defaultdict
 
 def get_desc():
     return 12, 'Day 12: Passage Pathing'
 
 def calc(log, values, mode):
-    paths = {}
+    paths = defaultdict(set)
+    small_rooms = set()
 
     for cur in values:
         if len(cur.strip()) > 0:
             cur = cur.split("-")
-            for x,y in ((cur[0], cur[1]), (cur[1], cur[0])):
-                if x not in paths:
-                    paths[x] = set()
-                paths[x].add(y)
+            for x, y in ((cur[0], cur[1]), (cur[1], cur[0])):
+                if x.lower() == x and x not in {"start", "end"}:
+                    small_rooms.add(x)
+
+                if y != "start":
+                    paths[x].add(y)
 
     trails = 0
-    todo = deque([(set(["start"]), None, "start")])
+    todo = deque([(set(["start"]), False, "start")])
     while len(todo) > 0:
         trail, dupe, cur = todo.pop()
         for x in paths[cur]:
-            if x != "start":
-                if x == "end":
-                    trails += 1
-                elif x.lower() == x:
-                    if mode == 1:
-                        if x not in trail:
-                            todo.append((trail | set([x]), dupe, x))
-                    else:
-                        if x not in trail:
-                            todo.append((trail | set([x]), dupe, x))
-                        else:
-                            if dupe is None:
-                                todo.append((trail, x, x))
-                else:
+            if x == "end":
+                trails += 1
+            elif x in small_rooms:
+                if x not in trail:
                     todo.append((trail | set([x]), dupe, x))
+                elif mode == 2:
+                    if not dupe:
+                        todo.append((trail, True, x))
+            else:
+                todo.append((trail | set([x]), dupe, x))
 
     return trails
 
@@ -52,6 +50,31 @@ def test(log):
 
     log.test(calc(log, values, 1), 10)
     log.test(calc(log, values, 2), 36)
+
+    values = log.decode_values("""
+        fs-end
+        he-DX
+        fs-he
+        start-DX
+        pj-DX
+        end-zg
+        zg-sl
+        zg-pj
+        pj-he
+        RW-he
+        fs-DX
+        pj-RW
+        zg-RW
+        start-pj
+        he-WI
+        zg-he
+        pj-fs
+        start-RW
+    """)
+
+    log.test(calc(log, values, 1), 226)
+    log.test(calc(log, values, 2), 3509)
+
 
 def run(log, values):
     log(calc(log, values, 1))
