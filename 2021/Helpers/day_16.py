8d39b90613bdf7c7041fb175800b286e1c4ab0d6
Scott Seligman <scott.seligman@gmail.com>
2021-12-15 22:14:17 -0800

    Add a way to show the code

--- 2021/Helpers/day_16.py
+++ 2021/Helpers/day_16.py
@@ -24,19 +24,71 @@ def decode(temp, results):
             val += x[1:]
             if x[0] == '0':
                 break
+        if results["print_formula"]:
+            results["formula"].append(str(int(val, 2)))
         return int(val, 2)
     else:
         length = get_bits(temp, 1)
         stack = []
+
+        if results["print_formula"]:
+            if pid == 0:
+                results["formula"].append("(")
+            elif pid == 1:
+                results["formula"].append("(")
+            elif pid == 2:
+                results["formula"].append("min([")
+            elif pid == 3:
+                results["formula"].append("max([")
+            elif pid in {5, 6, 7}:
+                results["formula"].append("(1 if (")
+
         if length == '0':
             sub_len = int(get_bits(temp, 15), 2)
             sub_temp = list(get_bits(temp, sub_len))
             while len(sub_temp) > 0:
                 stack.append(decode(sub_temp, results))
+                if results["print_formula"]:
+                    if pid == 5:
+                        results["formula"].append(">")
+                    elif pid == 6:
+                        results["formula"].append("<")
+                    elif pid == 7:
+                        results["formula"].append("==")
+                    elif pid == 0:
+                        results["formula"].append("+")
+                    elif pid == 1:
+                        results["formula"].append("*")
+                    else:
+                        results["formula"].append(",")
+            if results["print_formula"]:
+                results["formula"].pop(-1)
         else:
             sub_count = int(get_bits(temp, 11), 2)
             for _ in range(sub_count):
                 stack.append(decode(temp, results))
+                if results["print_formula"]:
+                    if pid == 5:
+                        results["formula"].append(">")
+                    elif pid == 6:
+                        results["formula"].append("<")
+                    elif pid == 7:
+                        results["formula"].append("==")
+                    elif pid == 0:
+                        results["formula"].append("+")
+                    elif pid == 1:
+                        results["formula"].append("*")
+                    else:
+                        results["formula"].append(",")
+            if results["print_formula"]:
+                results["formula"].pop(-1)
+
+        if pid in {5, 6, 7}:
+            results["formula"].append(") else 0)")
+        elif pid in {2, 3}:
+            results["formula"].append("])")
+        else:
+            results["formula"].append(")")
         if pid == 0:
             return sum(stack)
         elif pid == 1:
@@ -52,19 +104,30 @@ def decode(temp, results):
         elif pid == 7:
             return 1 if stack[0] == stack[1] else 0
 
-
-def calc(log, values, mode):
+def calc(log, values, mode, print_formula=False):
     temp = bin(int(values[0], 16))[2:]
     temp = list(("0" * (len(values[0]) * 4) + temp)[-len(values[0]) * 4:])
     results = {
         "mode": mode,
         "version_sum": 0,
+        "print_formula": print_formula,
+        "formula": [],
     }
     temp = decode(temp, results)
+
+    if print_formula:
+        log(" ".join(results["formula"]))
+
     if mode == 1:
         return results["version_sum"]
     else:
         return temp
+
+def other_show_formula(describe, values):
+    if describe:
+        return "Show the formula used"
+    from dummylog import DummyLog
+    calc(DummyLog(), values, 2, print_formula=True)
 
 def test(log):
     log.test(calc(log, ["620080001611562C8802118E34"], 1), 12)
