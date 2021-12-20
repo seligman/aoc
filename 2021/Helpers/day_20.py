bc53a12fa28dd687d9b87c5a399aca0fbfb30463
Scott Seligman <scott.seligman@gmail.com>
2021-12-19 21:34:19 -0800

    Add an animation

--- 2021/Helpers/day_20.py
+++ 2021/Helpers/day_20.py
@@ -3,7 +3,7 @@
 def get_desc():
     return 20, 'Day 20: Trench Map'
 
-def calc(log, values, mode):
+def calc(log, values, mode, draw=False):
     values = values[:]
     codes = values.pop(0).replace("#", "1").replace(".", "0")
     values.pop(0)
@@ -11,6 +11,11 @@ def calc(log, values, mode):
     from grid import Grid
     grid = Grid.from_text([x.replace("#", "1").replace(".", "0") for x in values])
     grid.default = "0"
+
+    if draw:
+        save = Grid()
+        save.grid = grid.grid.copy()
+        save.save_frame()
 
     for _ in range(2 if mode == 1 else 50):
         next_grid = Grid()
@@ -23,8 +28,22 @@ def calc(log, values, mode):
                     z += grid[i % 3 - 1 + x, i // 3 - 1 + y]
                 next_grid[x, y] = codes[int(z, 2)]
         grid = next_grid
+        save.grid = grid.grid.copy()
+        save.save_frame()
+
+    if draw:
+        save.draw_frames(color_map={"0": (0, 0, 0), "1": (255, 255, 255)}, cell_size=(2, 2), show_lines=False)
 
     return len("".join(x for x in grid.grid.values() if x == "1"))
+
+def other_draw(describe, values):
+    if describe:
+        return "Draw this"
+    from dummylog import DummyLog
+    import animate
+    animate.prep()
+    calc(DummyLog(), values, 2, draw=True)
+    animate.create_mp4(get_desc(), rate=5, final_secs=5)
 
 def test(log):
     values = log.decode_values("""
