b6c2e5a4cf78eef1532f93e76626a1b3f5aaae81
Scott Seligman <scott.seligman@gmail.com>
2022-12-07 21:11:24 -0800

    Done with day 8

---
+++ 2022/Helpers/day_08.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+DAY_NUM = 8
+DAY_DESC = 'Day 8: Treetop Tree House'
+
+def calc(log, values, mode):
+    from grid import Grid
+    grid = Grid.from_text(values)
+    grid.default = None
+
+    count = 0
+    best = 0
+    for x in range(0, grid.axis_max(0)+1):
+        for y in range(0, grid.axis_max(1)+1):
+            bads = []
+            dist = []
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                dist.append(0)
+                bad = False
+                start = grid[(x,y)]
+                ox, oy = x, y
+                while True:
+                    ox += dx
+                    oy += dy
+                    if grid[(ox, oy)] is None:
+                        break
+                    dist[-1] += 1
+                    if grid[(ox, oy)] >= start:
+                        bad = True
+                        break
+                bads.append(bad)
+            best = max(best, dist[0] * dist[1] * dist[2] * dist[3])
+            if False in bads:
+                count += 1
+
+    if mode == 1:
+        return count
+    else:
+        return best
+
+def test(log):
+    values = log.decode_values("""
+        30373
+        25512
+        65332
+        33549
+        35390
+    """)
+
+    log.test(calc(log, values, 1), 21)
+    log.test(calc(log, values, 2), 8)
+
+def run(log, values):
+    log(calc(log, values, 1))
+    log(calc(log, values, 2))
+
+if __name__ == "__main__":
+    import sys, os
+    def find_input_file():
+        for fn in sys.argv[1:] + ["input.txt", f"day_{DAY_NUM:0d}_input.txt", f"day_{DAY_NUM:02d}_input.txt"]:
+            for dn in [[], ["Puzzles"], ["..", "Puzzles"]]:
+                cur = os.path.join(*(dn + [fn]))
+                if os.path.isfile(cur): return cur
+    fn = find_input_file()
+    if fn is None: print("Unable to find input file!"); exit(1)
+    with open(fn) as f: values = [x.strip() for x in f.readlines()]
+    print(f"Running day {DAY_DESC}:")
+    run(print, values)
