131b685f2647a79892a61d3c362f920a56610889
Scott Seligman <scott.seligman@gmail.com>
2022-12-17 10:19:28 -0800

    Adding a speed up animation

--- 2022/Helpers/day_17.py
+++ 2022/Helpers/day_17.py
@@ -1,62 +1,81 @@
 #!/usr/bin/env python3
 
 # Animation: https://youtu.be/dMAxp1nmmiI
+# Speed Up: https://youtu.be/Hrsan4LFvDo
 
 DAY_NUM = 17
 DAY_DESC = 'Day 17: Pyroclastic Flow'
 
-from collections import deque
+from collections import deque, namedtuple
 
-_frame = 0
-_target_off = 1
-_off = 1
-_source_code = None
-def save_frame(grid, shape, blocks):
-    from grid import Grid
-    from PIL import Image, ImageDraw
+class DrawHelper:
+    def __init__(self, grid, fast):
+        self.frame = 0
+        self.target_off = 1
+        self.off = 1
+        self.source_code = grid.get_font(10)
+        self.skip = 0
+        self.fast = fast
+        self.speed = 0.25
 
-    global _frame, _target_off, _off, _source_code
-    if _target_off < _off:
-        _off -= 0.25
+    def save_frame(self, grid, shape, blocks, final_frame=False):
+        if not final_frame:
+            if self.skip > 0:
+                self.skip -= 1
+                return
+        from grid import Grid
+        from PIL import Image, ImageDraw
 
-    temp = Grid()
-    if _source_code is None:
-        _source_code = temp.get_font(10)
-    if _off == 1:
-        for x in range(-1, 8):
-            temp[(x, 1)] = "#"
+        if self.target_off < self.off:
+            self.off -= self.speed
 
-    if len(grid.grid) == 0:
-        max_height = 0
-    else:
-        max_height = (-min(grid.y_range())) + 1
-    for y in range(int(_off), int(_off)-40, -1):
-        temp[(-1, y)] = "#"
-        temp[(7, y)] = "#"
+        temp = Grid()
+        if self.off == 1:
+            for x in range(-1, 8):
+                temp[(x, 1)] = "#"
 
-        for x in range(7):
-            if grid[(x, y)] == "#":
-                _target_off = min(_target_off, y + 25)
-                temp[(x, y)] = "#"
+        if len(grid.grid) == 0:
+            max_height = 0
+        else:
+            max_height = -grid.axis_min(1) + 1
+        for y in range(int(self.off), int(self.off)-40, -1):
+            temp[(-1, y)] = "#"
+            temp[(7, y)] = "#"
 
-    if shape is not None:
-        for pt in shape:
-            if pt.y > int(_off) - 40 and pt.y <= int(_off):
-                temp[pt] = "Star"
+            for x in range(7):
+                if grid[(x, y)] == "#":
+                    self.target_off = min(self.target_off, y + 25)
+                    temp[(x, y)] = "#"
 
-    temp[(20, int(_off))] = 0
+        if shape is not None:
+            for pt in shape:
+                if pt.y > int(self.off) - 40 and pt.y <= int(self.off):
+                    temp[pt] = "Star"
 
-    im = temp.draw_grid(return_image=True)
-    im2 = Image.new('RGB', (im.width, im.height - 40))
-    im2.paste(im, (0, -20-10-int((_off - int(_off)) * 10)))
-    dr = ImageDraw.Draw(im2)
-    dr.text((125, 10), f"Blocks: {blocks:2,}\nHeight: {max_height:2,}", (255, 255, 255), _source_code)
-    del dr
+        temp[(20, int(self.off))] = 0
 
-    im2.save(f"frame_{_frame:05d}.png")
-    if _frame % 250 == 0:
-        print(f"Saved frame {_frame}, with {blocks} blocks")
-    _frame += 1
+        im = temp.draw_grid(return_image=True)
+        im2 = Image.new('RGB', (im.width, im.height - 40))
+        im2.paste(im, (0, -20-10-int((self.off - int(self.off)) * 10)))
+        dr = ImageDraw.Draw(im2)
+        dr.text((125, 10), f"Blocks: {blocks:2,}\nHeight: {max_height:2,}", (255, 255, 255), self.source_code)
+        del dr
+
+        im2.save(f"frame_{self.frame:05d}.png")
+        if self.frame % 250 == 0:
+            print(f"Saved frame {self.frame}, with {blocks} blocks")
+        if self.fast:
+            if blocks >= (2): self.skip, self.speed = 1, 0.5
+            if blocks >= (2 + 4): self.skip, self.speed = 2, 1
+            if blocks >= (2 + 4 + 8): self.skip, self.speed = 4, 2
+            if blocks >= (2 + 4 + 8 + 16): self.skip, self.speed = 8, 4
+            if blocks >= (2 + 4 + 8 + 16 + 32): self.skip, self.speed = 16, 8
+            if blocks >= (2022 - (2 + 4 + 8 + 16 + 32)): self.skip, self.speed = 8, 4
+            if blocks >= (2022 - (2 + 4 + 8 + 16)): self.skip, self.speed = 4, 2
+            if blocks >= (2022 - (2 + 4 + 8)): self.skip, self.speed = 2, 1
+            if blocks >= (2022 - (2 + 4)): self.skip, self.speed = 1, 0.5
+            if blocks >= (2022 - (2)): self.skip, self.speed = 0, 0.25
+        self.frame += 1
 
 def other_draw(describe, values):
     if describe:
@@ -64,10 +83,19 @@ def other_draw(describe, values):
     from dummylog import DummyLog
     import animate
     animate.prep()
-    calc(DummyLog(), values, 1, draw=True)
+    calc(DummyLog(), values, 1, draw=True, fast_mode=False)
     animate.create_mp4(DAY_NUM, rate=30, final_secs=5)
 
-def calc(log, values, mode, draw=False):
+def other_draw_fast(describe, values):
+    if describe:
+        return "Draw this, speeding up"
+    from dummylog import DummyLog
+    import animate
+    animate.prep()
+    calc(DummyLog(), values, 1, draw=True, fast_mode=True)
+    animate.create_mp4(DAY_NUM, rate=30, final_secs=15)
+
+def calc(log, values, mode, draw=False, fast_mode=False):
     from grid import Grid, Point
 
     grid = Grid()
@@ -80,12 +108,15 @@ def calc(log, values, mode, draw=False):
     step = 0
     rotates = 0
     seen = {}
+    SeenItem = namedtuple('SeenItem', ['height', 'step'])
 
     bail = -1
     max_steps = 2022 if mode == 1 else 1000000000000
     add = 0
     offset = 0
     blocks = 0
+    if draw:
+        helper = DrawHelper(grid, fast_mode)
 
     for step in range(max_steps):
         if bail > 0:
@@ -96,7 +127,7 @@ def calc(log, values, mode, draw=False):
         if len(grid.grid) == 0:
             y = -3
         else:
-            y = min(grid.y_range()) - 4
+            y = grid.axis_min(1) - 4
 
         step += offset
         if (step % 5) == 0:
@@ -110,28 +141,25 @@ def calc(log, values, mode, draw=False):
         elif (step % 5) == 4:
             shape = [(2, 0), (3, 0), (2, -1), (3, -1)]
         
-        shape = [Point(*pt) for pt in shape]
-        shape = [pt + (0, y) for pt in shape]
+        shape = [Point(px, py + y) for px, py in shape]
 
-        key = (step % 5, rotates % len(steps))
-
-        if key not in seen:
-            seen[key] = []
-        
-        if len(grid.grid) > 0 and bail < 0:
-            if len(seen[key]) == 6:
-                steps_taken = seen[key][2+2] - seen[key][0+2]
-                lines_added = seen[key][3+2] - seen[key][1+2]
-
-                remaining_cycles = (max_steps - step) // steps_taken
-                bail = (max_steps - step) % steps_taken + 1
-                add += lines_added * int(remaining_cycles)
-                offset = -1
-                seen = {}
-                continue
-            else:
-                seen[key].append(step)
-                seen[key].append(max(grid.y_range()) - min(grid.y_range()))
+        if bail < 0 and len(grid.grid) > 0:
+            cur_height = -grid.axis_min(1)
+            if cur_height > 500:
+                key = (step % 5, rotates % len(steps))
+                temp = seen.get(key, None)
+                if temp is None:
+                    temp = SeenItem(cur_height, step)
+                    seen[key] = temp
+                else:
+                    lines_added = cur_height - temp.height
+                    steps_taken = step - temp.step
+                    remaining_cycles = (max_steps - step) // steps_taken
+                    bail = (max_steps - step) % steps_taken + 1
+                    add += lines_added * int(remaining_cycles)
+                    offset = -1
+                    seen = {}
+                    continue
 
         while True:
             rotates += 1
@@ -153,7 +181,7 @@ def calc(log, values, mode, draw=False):
                 shape = [pt + dir for pt in shape]
 
             if draw:
-                save_frame(grid, shape, blocks)
+                helper.save_frame(grid, shape, blocks)
                 
             hit = False
             for pt in shape:
@@ -167,14 +195,16 @@ def calc(log, values, mode, draw=False):
                     grid[pt] = "#"
                 blocks += 1
                 if draw:
-                    save_frame(grid, None, blocks)
+                    helper.save_frame(grid, None, blocks)
                 break
             else:
                 if draw:
-                    save_frame(grid, shape, blocks)
+                    helper.save_frame(grid, shape, blocks)
                 shape = [pt + (0, 1) for pt in shape]
 
-    return max(grid.y_range()) - min(grid.y_range()) + add + 1
+    if draw:
+        helper.save_frame(grid, None, blocks, True)
+    return -grid.axis_min(1) + add + 1
 
 def test(log):
     values = log.decode_values("""
