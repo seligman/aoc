c9fc14e610c9345864d8fdd3f68eea4c09fa725c
Scott Seligman <scott.seligman@gmail.com>
2022-12-22 21:41:12 -0800

    Done with day 23

---
+++ 2022/Helpers/day_23.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+DAY_NUM = 23
+DAY_DESC = 'Day 23: Unstable Diffusion'
+
+def calc(log, values, mode):
+    # TODO: Delete or use these
+    # from parsers import get_ints, get_floats
+    from grid import Grid, Point
+    # from program import Program
+    grid = Grid.from_text(values)
+    # program = Program(values)
+
+    checks = [
+        [( 0,-1), [( 0, -1), ( 1, -1), (-1, -1)]],
+        [( 0, 1), [( 0,  1), ( 1,  1), (-1,  1)]],
+        [(-1, 0), [(-1,  0), (-1,  1), (-1, -1)]],
+        [( 1, 0), [( 1,  0), ( 1,  1), ( 1, -1)]],
+    ]
+
+    round = 1
+    while True:
+        elves = []
+        any_moved = 0
+        for x, y in grid.grid.keys():
+            if grid[(x, y)] == "#":
+                elves.append({
+                    "pt": Point(x,y),
+                    "move": None,
+                })
+        for elf in elves:
+            others = 0
+            for pt in [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]:
+                if grid[elf["pt"] + pt] == "#":
+                    others += 1
+            elf["can_move"] = others > 0
+
+        from collections import defaultdict
+        counts = defaultdict(int)
+        for elf in elves:
+            for target, check in checks:
+                if elf["can_move"]:
+                    any_elves = False
+                    for test in check:
+                        if grid[elf["pt"] + test] == "#":
+                            any_elves = True
+                            break
+                    if not any_elves:
+                        elf["move"] = elf["pt"] + target
+                        counts[elf["move"].tuple] += 1
+                        break
+        for elf in elves:
+            if elf["move"] is not None:
+                if counts[elf["move"].tuple] == 1:
+                    grid[elf["pt"]] = 0
+                    grid[elf["move"]] = "#"
+                    any_moved += 1
+        
+        checks.append(checks.pop(0))
+        if mode == 1:
+            if round == 10:
+                break
+        else:
+            if any_moved == 0:
+                return round 
+        round += 1
+
+
+    clean = Grid()
+    for x, y in grid.grid.keys():
+        if grid[(x, y)] == "#":
+            clean[(x, y)] = "#"
+
+    count = 0
+    for x in clean.x_range():
+        for y in clean.y_range():
+            if clean[(x, y)] != "#":
+                count += 1
+
+    # TODO
+    return count
+
+def test(log):
+    values = log.decode_values("""
+        ....#..
+        ..###.#
+        #...#.#
+        .#...##
+        #.###..
+        ##.#.##
+        .#..#..
+    """)
+
+    log.test(calc(log, values, 1), '110')
+    log.test(calc(log, values, 2), '20')
+
+def run(log, values):
+    log(calc(log, values, 1))
+    log(calc(log, values, 2))
+
+if __name__ == "__main__":
+    import sys, os
+    def find_input_file():
+        for fn in sys.argv[1:] + ["input.txt", f"day_{DAY_NUM:0d}_input.txt", f"day_{DAY_NUM:02d}_input.txt"]:
+            for dn in [[], ["Puzzles"], ["..", "Puzzles"]]:
+                cur = os.path.join(*(dn + [fn]))
+                if os.path.isfile(cur): return cur
+    fn = find_input_file()
+    if fn is None: print("Unable to find input file!\nSpecify filename on command line"); exit(1)
+    print(f"Using '{fn}' as input file:")
+    with open(fn) as f: values = [x.strip("\r\n") for x in f.readlines()]
+    print(f"Running day {DAY_DESC}:")
+    run(print, values)
