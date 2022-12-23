4337ea8b6568b7ef5324621773f4cee7f085764c
Scott Seligman <scott.seligman@gmail.com>
2022-12-23 11:58:01 -0800

    Clean up

--- 2022/Helpers/day_23.py
+++ 2022/Helpers/day_23.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 # Animation: https://youtu.be/s3xn8bOo6pA
+#            https://youtu.be/yPZwSp3Te_M (Smoothed version)
+
+from collections import defaultdict
 
 DAY_NUM = 23
 DAY_DESC = 'Day 23: Unstable Diffusion'
 
+class Elf:
+    __slots__ = ['at', 'move', 'can_move', 'moved']
+    def __init__(self, at):
+        self.at = at
+        self.move = None
+        self.can_move = False
+        self.moved = False
+
 def calc(log, values, mode, draw=False):
+    elves = []
+
     from grid import Grid, Point
-    grid = Grid.from_text(values)
+    if draw:
+        grid = Grid()
+        trail = {}
+
+    for y, row in enumerate(values):
+        for x, cell in enumerate(row):
+            if cell == "#":
+                elves.append(Elf(Point(x, y)))
 
     checks = [
         [( 0,-1), [( 0, -1), ( 1, -1), (-1, -1)]],
@@ -15,83 +35,77 @@ def calc(log, values, mode, draw=False):
         [(-1, 0), [(-1,  0), (-1,  1), (-1, -1)]],
         [( 1, 0), [( 1,  0), ( 1,  1), ( 1, -1)]],
     ]
+    dirs = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+    for check in checks:
+        for i in range(len(check[1])):
+            check[1][i] = dirs.index(check[1][i])
 
     round = 1
+    positions = set(x.at.tuple for x in elves)
     while True:
-        elves = []
-        any_moved = 0
-        for x, y in grid.grid.keys():
-            if grid[(x, y)] == "#":
-                elves.append({
-                    "pt": Point(x,y),
-                    "move": None,
-                })
-        for elf in elves:
-            others = 0
-            for pt in [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]:
-                if grid[elf["pt"] + pt] == "#":
-                    others += 1
-            elf["can_move"] = others > 0
-
-        from collections import defaultdict
-        counts = defaultdict(int)
-        if draw:
-            moved = set()
+        wants_to_move_to = defaultdict(int)
 
         for elf in elves:
-            for target, check in checks:
-                if elf["can_move"]:
-                    any_elves = False
-                    for test in check:
-                        if grid[elf["pt"] + test] == "#":
-                            any_elves = True
-                            break
-                    if not any_elves:
-                        elf["move"] = elf["pt"] + target
-                        counts[elf["move"].tuple] += 1
+            elf.moved = False
+            elf.move = None
+
+            has_elf = [((elf.at + pt).tuple in positions) for pt in dirs]
+            if any(has_elf):
+                for target, check in checks:
+                    if not any(has_elf[i] for i in check):
+                        elf.move = (elf.at + target)
+                        wants_to_move_to[elf.move.tuple] += 1
                         break
+
+        any_moved = 0
         for elf in elves:
-            if elf["move"] is not None:
-                if counts[elf["move"].tuple] == 1:
-                    grid[elf["pt"]] = 0
-                    grid[elf["move"]] = "#"
-                    if draw:
-                        moved.add(elf["move"].tuple)
+            if elf.move is not None:
+                if wants_to_move_to[elf.move.tuple] == 1:
+                    positions.remove(elf.at.tuple)
+                    positions.add(elf.move.tuple)
+                    elf.at = elf.move
+                    elf.moved = True
                     any_moved += 1
         
         checks.append(checks.pop(0))
         if mode == 1:
             if round == 10:
-                break
+                min_x = min(x.at.x for x in elves)
+                min_y = min(x.at.y for x in elves)
+                max_x = max(x.at.x for x in elves) + 1
+                max_y = max(x.at.y for x in elves) + 1
+                return ((max_x - min_x) * (max_y - min_y)) - len(elves)
         else:
             if draw:
-                for pt in grid.grid.keys():
-                    if grid[pt] == "#":
-                        if pt not in moved:
-                            grid[pt] = "star"
+                active = set(x.at.tuple for x in elves)
+                for key in trail:
+                    trail[key] = max(10 if key in active else 0, trail[key] - 1)
+                for elf in elves:
+                    if trail.get(elf.at.tuple, 0) < 10:
+                        trail[elf.at.tuple] = 12
+                
+                for key, value in trail.items():
+                    rgb = {12: 128, 11: 192, 10: 255}.get(value, int((value / 10) * 192))
+                    grid[key] = [None, (rgb, rgb, rgb)]
                 grid.save_frame()
-                for pt in grid.grid.keys():
-                    if grid[pt] == "star":
-                        grid[pt] = "#"
             if any_moved == 0:
                 if draw:
+                    while True:
+                        found = False
+                        for key in trail:
+                            if trail[key] != 10 and trail[key] > 0:
+                                trail[key] = max(0, trail[key] - 1)
+                                found = True
+                        if not found:
+                            break
+                        for key, value in trail.items():
+                            rgb = {12: 128, 11: 192, 10: 255}.get(value, int((value / 10) * 192))
+                            grid[key] = [None, (rgb, rgb, rgb)]
+                        grid.save_frame()
+
                     grid.draw_frames()
-                return round 
+                return round
         round += 1
-
-
-    clean = Grid()
-    for x, y in grid.grid.keys():
-        if grid[(x, y)] == "#":
-            clean[(x, y)] = "#"
-
-    count = 0
-    for x in clean.x_range():
-        for y in clean.y_range():
-            if clean[(x, y)] != "#":
-                count += 1
-
-    return count
 
 def other_draw(describe, values):
     if describe:
