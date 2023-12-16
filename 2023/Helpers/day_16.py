c36d619b3ff00c57e37aa59157237f3f299c7646
Scott Seligman <scott.seligman@gmail.com>
2023-12-15 21:53:07 -0800

    Clean up

--- 2023/Helpers/day_16.py
+++ 2023/Helpers/day_16.py
@@ -11,45 +11,39 @@ def calc(log, values, mode):
     def get_starts():
         for y in grid.y_range():
             yield deque([(0, y, 1, 0)])
-            if mode == 1:
-                break
+            if mode == 1: return
             yield deque([(grid.axis_max(0), y, -1, 0)])
-        if mode == 2:
-            for x in grid.x_range():
-                yield deque([(x, 0, 0, 1)])
-                yield deque([(x, grid.axis_max(1), 0, -1)])
+        for x in grid.x_range():
+            yield deque([(x, 0, 0, 1)])
+            yield deque([(x, grid.axis_max(1), 0, -1)])
 
     best = 0
     for todo in get_starts():
-        seen = set()
         energy = set()
         energy.add((todo[0][0], todo[0][1]))
+        seen = set()
+
+        def add_if_new(x, y, ox, oy):
+            key = x, y, ox, oy
+            if key not in seen:
+                seen.add(key)
+                todo.append(key)
+
         while len(todo) > 0:
             x, y, ox, oy = todo.pop()
-            if (x, y, ox, oy) not in seen:
-                seen.add((x, y, ox, oy))
-                x += ox
-                y += oy
-                if 0 <= x <= grid.axis_max(0) and 0 <= y <= grid.axis_max(1):
-                    energy.add((x, y))
-                    if grid[x, y] == "|" and oy == 0:
-                        todo.append((x, y, 0, 1))
-                        todo.append((x, y, 0, -1))
-                    elif grid[x, y] == "-" and ox == 0:
-                        todo.append((x, y, 1, 0))
-                        todo.append((x, y, -1, 0))
-                    else:
-                        if grid[x, y] == "/":
-                            if (ox, oy) == (1, 0): ox, oy = 0, -1
-                            elif (ox, oy) == (-1, 0): ox, oy = 0, 1
-                            elif (ox, oy) == (0, -1): ox, oy = 1, 0
-                            elif (ox, oy) == (0, 1): ox, oy = -1, 0
-                        elif grid[x, y] == "\\":
-                            if (ox, oy) == (1, 0): ox, oy = 0, 1
-                            elif (ox, oy) == (-1, 0): ox, oy = 0, -1
-                            elif (ox, oy) == (0, -1): ox, oy = -1, 0
-                            elif (ox, oy) == (0, 1): ox, oy = 1, 0
-                        todo.append((x, y, ox, oy))
+            x, y = x + ox, y + oy
+            if 0 <= x <= grid.axis_max(0) and 0 <= y <= grid.axis_max(1):
+                energy.add((x, y))
+                val = grid[x, y]
+                if (val == "|" and oy == 0) or (val == "-" and ox == 0):
+                    add_if_new(x, y, abs(oy), abs(ox))
+                    add_if_new(x, y, -abs(oy), -abs(ox))
+                else:
+                    if val == "/":
+                        ox, oy = -oy, -ox
+                    elif val == "\\":
+                        ox, oy = oy, ox
+                    add_if_new(x, y, ox, oy)
     
         best = max(best, len(energy))
 
