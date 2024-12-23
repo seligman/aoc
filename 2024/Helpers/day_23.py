76f7d38812ff85f2de2ad92fd28e16e240260052
Scott Seligman <scott.seligman@gmail.com>
2024-12-22 21:26:24 -0800

    Done with day 23

---
+++ 2024/Helpers/day_23.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+DAY_NUM = 23
+DAY_DESC = 'Day 23: LAN Party'
+
+def calc(log, values, mode):
+    # TODO: Delete or use these
+    # from parsers import get_ints, get_floats
+    # from grid import Grid, Point
+    # grid = Grid.from_text(values)
+    # from program import Program
+    # program = Program(values)
+
+    connected = set()
+    computers = set()
+    for row in values:
+        row = row.split("-")
+        connected.add((row[0], row[1]))
+        connected.add((row[1], row[0]))
+        computers.add(row[0])
+        computers.add(row[1])
+    
+    computers = list(sorted(computers))
+
+    if mode == 1:
+        ret = 0
+        for ai, a in enumerate(computers):
+            for bi, b in enumerate(computers):
+                if bi > ai:
+                    for ci, c in enumerate(computers):
+                        if ci > bi:
+                            if (a, b) in connected and (a, c) in connected and (b, c) in connected:
+                                if a.startswith("t") or b.startswith("t") or c.startswith("t"):
+                                    ret += 1
+        return ret
+    else:
+        import itertools
+        from collections import deque
+        todo = deque([x for x in itertools.combinations(computers, 2) if x in connected])
+        best = tuple()
+        while len(todo) > 0:
+            cur = todo.popleft()
+            if len(cur) > len(best):
+                best = cur
+            for x in computers:
+                if x > cur[-1]:
+                    good = True
+                    for y in cur:
+                        if (x, y) not in connected:
+                            good = False
+                            break
+                    if good:
+                        todo.append(cur + (x,))
+        return ",".join(sorted(best))
+        
+
+
+
+
+    # TODO
+
+def test(log):
+    values = log.decode_values("""
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn
+    """)
+
+    log.test(calc(log, values, 1), '7')
+    log.test(calc(log, values, 2), 'co,de,ka,ta')
+
+def run(log, values):
+    log(calc(log, values, 1))
+    log(calc(log, values, 2))
+
+if __name__ == "__main__":
+    import sys, os
+    def find_input_file():
+        for fn in sys.argv[1:] + ["input.txt", f"day_{DAY_NUM:0d}_input.txt", f"day_{DAY_NUM:02d}_input.txt"]:
+            for dn in ["", "Puzzles", "../Puzzles", "../../private/inputs/2024/Puzzles"]:
+                cur = os.path.join(*(dn.split("/") + [fn]))
+                if os.path.isfile(cur): return cur
+    fn = find_input_file()
+    if fn is None: print("Unable to find input file!\nSpecify filename on command line"); exit(1)
+    print(f"Using '{fn}' as input file:")
+    with open(fn) as f: values = [x.strip("\r\n") for x in f.readlines()]
+    print(f"Running day {DAY_DESC}:")
+    run(print, values)
